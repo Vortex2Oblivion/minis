69f1de5ac10fe668960ae98bada719049d3b2c8c
Vortex2Oblivion <73261680+Vortex2Oblivion@users.noreply.github.com>
2025-04-19 09:17:17 -0400

    Init

---
+++ README.md
@@ -0,0 +1,32 @@
+<p align="center">
+    MINIS
+</p>
+<p align="center">  
+    <img src="minis/sparkles.png" width="54"/>
+    <img src="minis/subspace.png" width="54"/>
+    <img src="minis/vinestaff.png" width="54"/>
+    <img src="minis/firebrand.png" width="54"/>
+    <img src="minis/sword.png" width="54"/>
+    <img src="minis/katana.png" width="54"/>
+    <img src="minis/coil.png" width="54"/>
+    <img src="minis/traffic.png" width="54"/>
+    <img src="minis/biograft.png" width="54"/>
+    <img src="minis/ducky.png" width="54"/>
+    <img src="minis/valk.png" width="54"/>
+    <img src="minis/venomshank.png" width="54"/>
+    <img src="minis/boombox.png" width="54"/>
+    <img src="minis/shuriken.png" width="54"/>
+    <img src="minis/scythe.png" width="54"/>
+    <img src="minis/medkit.png" width="54"/>
+    <img src="minis/broker.png" width="54"/>
+    <img src="minis/slingshit.png" width="54"/>
+    <img src="minis/hyperlaser.png" width="54"/>
+    <img src="minis/rocket.png" width="54"/>
+    <img src="minis/dom.png" width="54"/>
+    <img src="minis/banhammer.png" width="54"/>
+    <img src="minis/mxbot.png" width="54"/>
+    <img src="minis/zuka.png" width="54"/>
+</p>
+
+Adds the [PHIGHTING!](https://www.roblox.com/games/7138009149/PHIGHTING-ALPHA) minis to your desktop to sit on your taskbar using Raylib.
+All minis drawn by [soda](https://bsky.app/profile/sodakettle.bsky.social)
---
+++ src/main.c
@@ -0,0 +1,89 @@
+#include <raylib.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(int argc, char * argv[])
+{
+    SetTraceLogLevel(LOG_NONE);
+
+    SetConfigFlags(FLAG_WINDOW_TRANSPARENT);
+    SetConfigFlags(FLAG_WINDOW_MOUSE_PASSTHROUGH);
+    SetConfigFlags(FLAG_WINDOW_TOPMOST);
+    SetConfigFlags(FLAG_MSAA_4X_HINT);
+    SetConfigFlags(FLAG_WINDOW_UNDECORATED);
+    
+    InitWindow(GetScreenWidth(), GetScreenHeight(), "MINIS");
+
+    SetTargetFPS(10);
+
+
+    SetWindowPosition(0, 30);
+
+    Texture minis[24] = {
+        LoadTexture("minis/sparkles.png"),
+        LoadTexture("minis/subspace.png"),
+        LoadTexture("minis/vinestaff.png"),
+        LoadTexture("minis/firebrand.png"),
+        LoadTexture("minis/sword.png"),
+        LoadTexture("minis/katana.png"),
+        LoadTexture("minis/coil.png"),
+        LoadTexture("minis/traffic.png"),
+        LoadTexture("minis/biograft.png"),
+        LoadTexture("minis/ducky.png"),
+        LoadTexture("minis/valk.png"),
+        LoadTexture("minis/venomshank.png"),
+        LoadTexture("minis/boombox.png"),
+        LoadTexture("minis/shuriken.png"),
+        LoadTexture("minis/scythe.png"),
+        LoadTexture("minis/medkit.png"),
+        LoadTexture("minis/broker.png"),
+        LoadTexture("minis/slingshit.png"),
+        LoadTexture("minis/hyperlaser.png"),
+        LoadTexture("minis/rocket.png"),
+        LoadTexture("minis/dom.png"),
+        LoadTexture("minis/banhammer.png"),
+        LoadTexture("minis/mxbot.png"),
+        LoadTexture("minis/zuka.png")
+    };
+
+    
+    int minisLen = sizeof(minis) / sizeof(minis[0]);
+    
+    srand((unsigned int)time(NULL)); 
+    int index = rand()%(minisLen);
+    Image icon = LoadImageFromTexture(minis[index]);
+
+    SetWindowIcon(icon);
+
+    UnloadImage(icon);
+
+    double scale = 1.3;
+
+    for (int i = 0; i < minisLen; i++)
+    {
+        int oldWidth = minis[i].width;
+        minis[i].height = minis[i].width = (int)((GetScreenWidth() / minisLen) * scale);
+        SetTextureFilter(minis[i], TEXTURE_FILTER_ANISOTROPIC_16X);
+    }
+    int miniOffset = 45;
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(BLANK);
+        for (int i = 0; i < minisLen; i++)
+        {
+            DrawTexture(minis[i], i * (int)(minis[i].width/scale), GetScreenHeight() - minis[i].height - miniOffset -30, WHITE);
+        }
+        if(IsKeyPressed(KEY_F1)){
+            if(miniOffset == 45){
+                miniOffset = 0;
+            }
+            else{
+                miniOffset = 45;
+            }
+        }
+        EndDrawing();
+    }
+    CloseWindow();
+    return 0;
+}
